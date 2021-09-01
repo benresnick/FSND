a0b120758b8e359a6466dd58b764beeb88011277
benresnick <79893647+benresnick@users.noreply.github.com>
2021-09-01 01:11:50 -0700

    Update __init__.py
    fix: fixed up some indentations

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -99,7 +99,7 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page.
   '''
-  @app.route("questions/<int:question_id>", methods=["DELETE"])
+  @app.route("/questions/<int:question_id>", methods=["DELETE"])
   def delete_question(question_id):
       try:
           question = Question.query.filter(Question.id == question_id).one_or_none()
@@ -171,17 +171,17 @@ def create_app(test_config=None):
   including 404 and 422.
   '''
   @app.errorhandler(404)
-    def not_found(error):
-        return (
-            jsonify({"success": False, "error": 404, "message": "resource not found"}),
-            404,
-        )
+  def not_found(error):
+    return (
+        jsonify({"success": False, "error": 404, "message": "resource not found"}),
+        404,
+    )
 
-    @app.errorhandler(422)
-    def unprocessable(error):
-        return (
-            jsonify({"success": False, "error": 422, "message": "unprocessable"}),
-            422,
-        )
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return (
+        jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+        422,
+    )
 
   return app
