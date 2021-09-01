2bf8a2e8e0a63e4f33a046f5a7022543d252df63
benresnick <79893647+benresnick@users.noreply.github.com>
2021-09-01 00:04:47 -0700

    Update test_flaskr.py
    feat: wrote test for
    Creating an endpoint to handle GET requests for all available categories.

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -33,6 +33,14 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get("/categories")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_categories"])
+        self.assertTrue(len(data["categories"]))
 
 
 # Make the tests conveniently executable
