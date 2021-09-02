5e3cd66ddee52b535236f5b4f4d4ffdf66bfcfaf
benresnick <79893647+benresnick@users.noreply.github.com>
2021-09-01 17:24:45 -0700

    Update test_flaskr.py
    feat: added tests for getting questions by categories and expected failures

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -92,6 +92,23 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data["success"], True)
         self.assertEqual(data["total_questions"], 0)
         self.assertEqual(len(data["questions"]), 0)
+
+    def test_get_questions_by_category(self):
+        res = self.client().get("/categories/2/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertEqual(len(data["questions"]), 4)
+
+    def test_404_if_category_does_not_exist(self):
+        res = self.client().get("/categories/1000/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
